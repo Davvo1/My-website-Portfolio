8265f71989c53f67965ad58c913b665669ff4895
Davvo1 <davidebonardi15@gmail.com>
2023-02-17 16:52:32 +0000

    added script

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,22 @@
-let submit = document.querySelector("#submit");
-
-submit.addEventListener("click", function() {
-    preventDefault();
-    let getContacted = document.querySelector("#get-contacted");
-    getContacted.innerHTML = Ciao;
-});
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_b3rfcg9";
+    const templateID = "template_wx43dua";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
