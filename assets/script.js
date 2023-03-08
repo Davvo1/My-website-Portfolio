fb1cdea3804839dc461292e919c6604d390e9ddc
Davvo1 <davidebonardi15@gmail.com>
2023-03-08 18:39:22 +0100

    restored changes

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,30 @@
+let submit = document.querySelector("#submit");
+var params = {
+  name: document.getElementById("name").value,
+  email: document.getElementById("email").value,
+  message: document.getElementById("message").value,
+};
+
 function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-  
+
+  if(!params) {
+    alert("You must fill the form!"); 
+    return;
+  }
+     else {
     const serviceID = "service_b3rfcg9";
     const templateID = "template_wx43dua";
-  
       emailjs.send(serviceID, templateID, params)
       .then(res=>{
           document.getElementById("name").value = "";
           document.getElementById("email").value = "";
           document.getElementById("message").value = "";
           console.log(res);
-          alert("Your message sent successfully!!")
-  
+          alert("Your message was sent successfully!");
+          document.querySelector("form").reset();
       })
       .catch(err=>console.log(err));
-  
+     }  
   }
+
+  submit.addEventListener("click", sendMail());
