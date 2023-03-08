50438994a6fff5f2f0f7807359fac234e7bee581
Davvo1 <davidebonardi15@gmail.com>
2023-03-08 19:16:29 +0100

    fixed script

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,32 @@
 let submit = document.querySelector("#submit");
-var params = {
-  name: document.getElementById("name").value,
-  email: document.getElementById("email").value,
-  message: document.getElementById("message").value,
-};
 
-function sendMail() {
 
-  if(!params) {
-    alert("You must fill the form!"); 
+function sendMail(e) {
+ console.log("funzione sendmail");
+ e.preventDefault();
+  var params = {
+    user_name: document.getElementById("name").value,
+    user_email: document.getElementById("email").value,
+    user_message: document.getElementById("message").value,
+  };
+  if (!params) {
+    alert("You must fill the form!");
     return;
   }
-     else {
+  else {
     const serviceID = "service_b3rfcg9";
     const templateID = "template_wx43dua";
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message was sent successfully!");
-          document.querySelector("form").reset();
+    emailjs.send(serviceID, templateID, params)
+      .then(res => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message was sent successfully!");
+        document.querySelector("form").reset();
       })
-      .catch(err=>console.log(err));
-     }  
+      .catch(err => console.log(err));
   }
+}
 
-  submit.addEventListener("click", sendMail());
+submit.addEventListener("click", sendMail);
